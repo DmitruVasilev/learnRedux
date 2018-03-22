135565d68c17ede11b36de55b4b67e0bce5b1e3b
Dmitriu Vasilev <mr.disproziu91@gmail.com>
2018-03-22 13:55:03 +0300

    Add toggleOpen

--- src/Article.js
+++ src/Article.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-const Article = ({article}) => (
-  <div>
-    <h3>{article.title}</h3>
-    <section>{article.text}</section>
-  </div>
-)
+class Article extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  render(){
+    const {article} = this.props
+    const {isOpen} = this.state
+    return (
+      <div>
+        <h3>{article.title}</h3>
+        <button onClick={this.toggleOpen}>{isOpen ? "close":"open"}</button>
+        {this.getBody()}
+      </div>
+    )
+  }
+
+  getBody(){
+    const {isOpen} = this.state
+    const {article} = this.props
+    if (!isOpen) return null
+    return <section>{article.text}</section>
+  }
+
+  toggleOpen=()=>{
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+}
 
 Article.propTypes = {
-  // test: PropTypes.string
+  article: PropTypes.shape({
+    text: PropTypes.string,
+    title: PropTypes.string
+  }).isRequired
 }
 
 export default Article
