cd35a014232b562191dd6cc43a6425ff8aa4d879
Dmitriu Vasilev <mr.disproziu91@gmail.com>
2018-04-19 14:08:28 +0300

    Add react-router-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Articles from './routes/Articles'
 import NotFoundPage from './routes/NotFoundPage'
@@ -6,43 +6,41 @@ import UserForm from './UserForm'
 import Filters from './Filters'
 import Counter from './Counter'
 import CommentsPage from './routes/CommentsPage'
+import history from '../history'
 import 'react-select/dist/react-select.css'
-import {BrowserRouter as Router, Route, NavLink, Switch, Redirect} from 'react-router-dom'
+import {Route, NavLink, Switch} from 'react-router-dom'
+import {ConnectedRouter} from 'react-router-redux'
 
 class App extends Component {
-    static propTypes = {
-
-    };
-
-    render() {
-        return (
-            <Router>
-              <div>
-                <div>
-                  <h2>main menu</h2>
-                  <div>
-                    <NavLink activeStyle={{color:"red"}} to='/counter' >counter</NavLink>
-                  </div>
-                  <div>
-                    <NavLink activeStyle={{color:"red"}} to='/articles' >articles</NavLink>
-                  </div>
-                  <div>
-                    <NavLink activeStyle={{color:"red"}} to='/filters' >filters</NavLink>
-                  </div>
-                </div>
-                <UserForm />
-                <Switch>
-                  <Route path="/counter" component={Counter} />
-                  <Route path="/filters" component={Filters} />
-                  <Route path="/articles" component={Articles} />
-                  <Route path='/comments' component = {CommentsPage} />
-                  {/*<Redirect from="/comments" to="/comments/1" />*/}
-                  <Route path="*" component={NotFoundPage} />
-                </Switch>
-              </div>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <ConnectedRouter history={history}>
+        <div>
+          <div>
+            <h2>main menu</h2>
+            <div>
+              <NavLink activeStyle={{color: "red"}} to='/counter'>counter</NavLink>
+            </div>
+            <div>
+              <NavLink activeStyle={{color: "red"}} to='/articles'>articles</NavLink>
+            </div>
+            <div>
+              <NavLink activeStyle={{color: "red"}} to='/filters'>filters</NavLink>
+            </div>
+          </div>
+          <UserForm/>
+          <Switch>
+            <Route path="/counter" component={Counter}/>
+            <Route path="/filters" component={Filters}/>
+            <Route path="/articles" component={Articles}/>
+            <Route path='/comments' component={CommentsPage}/>
+            {/*<Redirect from="/comments" to="/comments/1" />*/}
+            <Route path="*" component={NotFoundPage}/>
+          </Switch>
+        </div>
+      </ConnectedRouter>
+    )
+  }
 }
 
 export default App
